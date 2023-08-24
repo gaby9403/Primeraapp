90f3d18f9d777ebcf88e331b55c823a87e744093
gaby9403 <143103562+gaby9403@users.noreply.github.com>
2023-08-24 09:59:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st
+
 st.title("El mejor título del universo")
+
 st.header("Las rosas son rojas")
-st write("William Shakespeare ")
+st.write("William Shakespeare")
 image = Image.open ('imagenapp.png')
 
 st.image(image, caption = 'El amor de tu vida')
