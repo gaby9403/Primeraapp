d526f67b2ffd7702f9fd2ca98543f668a78c9c23
gaby9403 <143103562+gaby9403@users.noreply.github.com>
2023-08-24 09:57:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
 import streamlit as st
-st.title('Papas con helado')
+st.title("El mejor título del universo")
+st.header("Las rosas son rojas")
+st write("– William Shakespeare ")
+image = Image.open ('imagenapp.png')
+
+st.image(image, caption = 'El amor de tu vida')
